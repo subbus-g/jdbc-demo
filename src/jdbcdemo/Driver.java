2708f66ded832fbe3a4732cf3dd9e6993d8df522
subbus <subbareddyg83@gmail.com>
2020-11-11 15:47:43 +0530

    added quering a database through jdbc

--- src/jdbcdemo/Driver.java
+++ src/jdbcdemo/Driver.java
@@ -5,7 +5,17 @@ import java.sql.*;
 class Driver {
     public static void main(String[] args) throws Exception 
     {
-    	System.out.println("Welcome to jdbc demo program");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "1234");
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT  ID,Name,Population FROM world.city LIMIT 10;");
+        while (resultSet.next()) 
+        {
+
+            System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getInt(3));
+        }
+
     }
 
 }
