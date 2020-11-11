be834755800c65c9e8360860ebc0b2a808398250
subbus <subbareddyg83@gmail.com>
2020-11-11 15:44:11 +0530

    added welcome message

---
+++ src/jdbcdemo/Driver.java
@@ -0,0 +1,11 @@
+package jdbcdemo;
+
+import java.sql.*;
+
+class Driver {
+    public static void main(String[] args) throws Exception 
+    {
+    	System.out.println("Welcome to jdbc demo program");
+    }
+
+}
