54446afeb435b3ceeb7e83c792789caa90962963
subbus <subbareddyg83@gmail.com>
2020-11-11 16:02:15 +0530

    added deletion of data in database

--- src/jdbcdemo/Driver.java
+++ src/jdbcdemo/Driver.java
@@ -4,44 +4,44 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
- 
+
 public class Driver {
- 
-    public static void main(String[] args) throws SQLException {
- 
-        String url = "jdbc:mysql://localhost:3306/demo";
-        String user = "root";
-        String password = "1234";
- 
-        Connection myConn = null;
-        Statement myStmt = null;
- 
-        try {
-            // 1. Get a connection to database
-            myConn = DriverManager.getConnection(url, user, password);
- 
-            // 2. Create a statement
-            myStmt = myConn.createStatement();
- 
-            // 3. Execute SQL query
-            String sql = "update employees set email='demo@gmail.com' where id=13";
- 
-            int rowsAffected = myStmt.executeUpdate(sql);
- 
-            System.out.println("Rows affected: " + rowsAffected);
-            System.out.println("Update complete.");
-        }
-        catch (Exception exc) {
-            exc.printStackTrace();
-        }
-        finally {
-            if (myStmt != null) {
-                myStmt.close();
-            }
- 
-            if (myConn != null) {
-                myConn.close();
-            }
-        }
-    }
+
+	public static void main(String[] args) throws SQLException {
+
+		String url = "jdbc:mysql://localhost:3306/demo";
+		String user = "root";
+		String password = "1234";
+
+		Connection myConn = null;
+		Statement myStmt = null;
+
+		try {
+			// 1. Get a connection to database
+			myConn = DriverManager.getConnection(url, user, password);
+
+			// 2. Create a statement
+			myStmt = myConn.createStatement();
+
+			// 3. Execute SQL query
+			String sql = "delete from employees where last_name='Brown'";
+
+			int rowsAffected = myStmt.executeUpdate(sql);
+
+			System.out.println("Rows affected: " + rowsAffected);
+			System.out.println("Delete complete.");
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			if (myStmt != null) {
+				myStmt.close();
+			}
+
+			if (myConn != null) {
+				myConn.close();
+			}
+		}
+	}
 }
